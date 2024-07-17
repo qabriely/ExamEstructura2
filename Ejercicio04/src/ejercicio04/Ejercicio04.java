586606d9eae9bd813ddd46f522638a31d88878ae
qabriely <gsonatore.0132@unimar.edu.ve>
2024-07-17 01:15:16 -0400

    Agregar clase ´CalculodePotencia´ y correción de errores

--- Ejercicio04/src/ejercicio04/Ejercicio04.java
+++ Ejercicio04/src/ejercicio04/Ejercicio04.java
@@ -10,20 +10,33 @@ class CalculodelFactorial {
             return n * calcFactorial(n - 1);
         }
     }
-    
-    public static void main(String[] args) {
-    Pila pila = new Pila();
-    Scanner scanner = new Scanner(System.in);
+}
 
-    System.out.println("Ingrese los valores (separados por ','). Presione Enter para finalizar:");
-    while (true) {
-        String valor = scanner.nextLine();
-        if (valor.isEmpty()) {
-            break;
+class CalculodePotencia {
+    static int calcPotencia(int base, int exponente) {
+        if (exponente == 0) {
+            return 1;
+        } else {
+            return base * calcPotencia(base, exponente - 1);
         }
-        pila.push(valor);
     }
+}
 
-    System.out.println("Los valores invertidos son:");
-    ImprInvert(pila);
+public class Ejercicio04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresar el número para poder calcular su factorial -> A:     ");
+        int a = scanner.nextInt();
+        int factorialA = CalculodelFactorial.calcFactorial(a);
+
+        System.out.print("Ingresar la base para calcular su potencia  -> B:    ");
+        int b = scanner.nextInt();
+        System.out.print("Ingresar el exponente para la potencia ->  B^(exponente):    ");
+        int exponenteB = scanner.nextInt();
+        int potenciaB = CalculodePotencia.calcPotencia(b, exponenteB);
+
+        int resultado = factorialA * potenciaB;
+        System.out.printf("%d! * %d^%d = %d\n", a, b, exponenteB, resultado);
+    }
 }
